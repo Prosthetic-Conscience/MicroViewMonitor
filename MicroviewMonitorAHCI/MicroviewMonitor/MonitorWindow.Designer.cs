f190e7b49e17174edec51aa892e8dd4d3095c1d8
ispringer76 <ispringer76@users.noreply.github.com>
2022-08-29 15:34:16 -0500

    Add files via upload

---
+++ MicroviewMonitorAHCI/MicroviewMonitor/MonitorWindow.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MicroviewMonitor
+    {
+    partial class MonitorWindow
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MonitorWindow));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cbPortName = new System.Windows.Forms.ComboBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbPortName
+            // 
+            this.cbPortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPortName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPortName.FormattingEnabled = true;
+            this.cbPortName.Location = new System.Drawing.Point(48, 21);
+            this.cbPortName.Name = "cbPortName";
+            this.cbPortName.Size = new System.Drawing.Size(235, 28);
+            this.cbPortName.TabIndex = 0;
+            this.cbPortName.DropDown += new System.EventHandler(this.cbPortName_DropDown);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipText = "Running";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "MicroView Monitor";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Image = global::MicroviewMonitor.Properties.Resources.Stop1NormalRed64x64;
+            this.btn_Stop.Location = new System.Drawing.Point(176, 55);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(98, 92);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Image = global::MicroviewMonitor.Properties.Resources.Play1Normal64x64;
+            this.btn_Start.Location = new System.Drawing.Point(48, 55);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(95, 92);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // MonitorWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 159);
+            this.Controls.Add(this.cbPortName);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MonitorWindow";
+            this.Text = "Microview CPU Monitor";
+            this.Resize += new System.EventHandler(this.MonitorWindow_Resize);
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cbPortName;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer;
+        }
+    }
+
---
+++ MicroviewMonitorAHCI/MicroviewMonitor/MonitorWindow.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Management;
+using OpenHardwareMonitor.Hardware;
+
+namespace MicroviewMonitor
+    {
+    public partial class MonitorWindow : Form
+        {
+        Boolean bAHCPI_Available;
+        Computer myComputer;
+
+
+        public MonitorWindow()
+            {
+            InitializeComponent();
+            bAHCPI_Available = test_For_AHCPI();
+            }
+
+        private void cbPortName_DropDown(object sender, EventArgs e)
+            {
+            // Get all serial ports name
+            string[] ports = SerialPort.GetPortNames();
+
+            // Clear comboBox list to get latest serial ports in each dropdown.
+            cbPortName.Items.Clear();
+            foreach (string port in ports)
+                {
+                // Output serial port to dropdown list.
+                cbPortName.Items.Add(port);
+                }
+            }
+
+        private void MonitorWindow_Resize(object sender, EventArgs e)
+            {
+            if (this.WindowState == FormWindowState.Minimized)
+                {
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(500);
+                this.ShowInTaskbar = false;
+                this.Hide();
+                }
+            }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+            {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+            }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+            {
+            if (serialPort1.IsOpen)
+                {
+                serialPort1.Close();
+                }
+            if (cbPortName.Text != null && cbPortName.Text != "")
+                {
+                serialPort1.PortName = cbPortName.Text; //cbPortname.Text set to PortName
+                serialPort1.BaudRate = 9600; //Baudrate is fixed
+                serialPort1.DataBits = 8; //Databits fixed
+                serialPort1.Parity = Parity.None; //Parity fixed
+                serialPort1.StopBits = StopBits.One; //Stop bits fixed
+
+                //If port isn't open, then open it
+                if (!serialPort1.IsOpen)
+                    {
+                    serialPort1.Open();
+                    }
+                //Enable timer
+                timer.Enabled = true;
+                }
+            if (cbPortName.Text == null || cbPortName.Text == "")
+                {
+                MessageBox.Show("Must select a serial port");
+                }
+            }
+
+        private string get_CPU_Load()
+            {
+            //Get CPU usage values using a WMI query
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            var cpuTimes = searcher.Get()
+                .Cast<ManagementObject>()
+                .Select(mo => new
+                    {
+                    Name = mo["Name"],
+                    Usage = mo["PercentProcessorTime"]
+                    }
+                )
+                .ToList();
+            //The '_Total' value represents the average usage across all cores,
+            //and is the best representation of overall CPU usage
+            var query = cpuTimes.Where(x => x.Name.ToString() == "_Total").Select(x => x.Usage);
+            var cpuUsage = query.SingleOrDefault();
+            return cpuUsage.ToString();
+            }
+
+        private Boolean test_For_AHCPI()
+            {
+            ManagementObjectSearcher searcherCPU = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            ManagementObjectSearcher searcherLoad = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            //MessageBox.Show("Found: " + searcher.Get().Count.ToString());
+            if ((searcherCPU.Get().Count > 0) && (searcherLoad.Get().Count > 0))
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+
+
+        private string get_CPU_Temp_AHCPI()
+            {
+            List<Double> result = new List<Double>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+            foreach (ManagementObject obj in searcher.Get())
+                {
+                Double temp = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                temp = ((temp - 2732) / 10);
+                temp = ((9.0 / 5.0) * temp) + 32;
+                result.Add(temp);
+                }
+            return Convert.ToInt32(result.Max()).ToString();            
+            //return result.Max().ToString();
+            }
+
+        private void timer_Tick(object sender, EventArgs e)
+            {
+            //Test to see if CPU temp available using AHCPI
+            //if(bAHCPI_Available == true)
+            //{
+            string currentTemp = get_CPU_Temp_AHCPI();
+            //}
+            // else if bAHCPI_Available == false)
+            //{
+            //
+            //}
+            string currentLoad = get_CPU_Load();
+            //MessageBox.Show(currentTemp);
+            //Console.WriteLine(currentTemp);
+            if (serialPort1.IsOpen)
+                {
+                System.Diagnostics.Debug.WriteLine("Temp " + currentTemp + "F : " + "Current Load " +currentLoad + "%");
+                serialPort1.WriteLine(currentTemp + ":" + currentLoad);
+                }
+            if (!serialPort1.IsOpen)
+                {
+                timer.Enabled = false;
+                MessageBox.Show("Serial port is closed");
+                }
+            }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+            {
+            timer.Enabled = false;
+            }
+
+
+
+        }
+    }
